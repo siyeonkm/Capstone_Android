d21c1a13910a4c9ba0f77a6cc154c73f32db8490
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2021-06-06 20:00:39 +0900

    Update AbnormalDownloadActivity.java

--- app/src/main/java/com/example/capstoneblackbox/AbnormalDownloadActivity.java
+++ app/src/main/java/com/example/capstoneblackbox/AbnormalDownloadActivity.java
@@ -29,7 +29,7 @@ public class AbnormalDownloadActivity extends AppCompatActivity {
         abcontext = this;
 
         ((MainActivity) mcontext).connectServerPost
-                .requestVideoGet("http://3.35.118.6/output");
+                .requestVideoGet("");
     }
 
     public void fromAbtoHomeActivity() {
